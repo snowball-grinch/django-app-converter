7bb62a478a84c6bbe29fcf121b3582967e8e726d
sadeq hashemi <sadeqmohammadh@gmail.com>
2021-03-28 16:31:11 +0430

    set up unit test and function for test centimetre to metre conversion and modify urlpatterns

--- converter/urls.py
+++ converter/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('length/', include('length/urls.py')),
+    path('length/', include('length.urls')),
 ]
--- length/tests.py
+++ length/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+# Client class:  This is a testing browser that enables us to make http requests within Django tests.
+from django.urls import reverse
+# reverse(viewname, urlconf=None, args=None, kwargs=None, current_app=None)
+# viewname can be a URL pattern name or the callable view object
+# ......
+#  using the named URL:
+# reverse('news-archive')
 
-# Create your tests here.
+#  passing a callable object
+#  (This is discouraged because you can't reverse namespaced views this way.)
+# from news import views
+# reverse(views.archive)"""
+# The reverse function is imported in order to return a url when the url’s name is passed in as an argument.
+
+
+class TestLengthConversion(TestCase):
+    """
+    This class contains tests that convert measurements from one
+    unit of measurement to another.
+    """
+    def setUp(self):
+        """
+        This method runs before the execution of each test case.
+        """
+        self.client = Client()
+        self.url = reverse("length:convert")
+
+    def test_centimetre_to_metre_conversion(self):
+        """
+        Tests conversion of centimetre measurements to metre.
+        """
+        data = {
+               "input_unit": "centimetre",
+               "output_unit": "metre",
+               "input_value": round(8096.894, 3)
+           }
+        response = self.client.get(self.url, data)
+        self.assertContains(response, 80.969)
